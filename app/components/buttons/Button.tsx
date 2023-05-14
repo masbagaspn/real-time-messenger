1f24fca380ff8b1e1ebfd09adb507f7623d04c1f
masbagaspn <masbagaspn@gmail.com>
2023-05-14 11:05:19 +0700

    feat: settings modal

--- app/components/buttons/Button.tsx
+++ app/components/buttons/Button.tsx
@@ -27,7 +27,7 @@ const Button: React.FC<ButtonProps> = ({
         type={type}
         disabled={disabled}
         className={clsx(
-            'flex justify-center rounded-md px-4 py-3 text-xs font-semibold',
+            'flex justify-center rounded-md px-3.5 py-2.5 text-xs font-semibold',
             'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
             disabled && 'opacity-50 cursor-default',
             fullWidth && 'w-full',
