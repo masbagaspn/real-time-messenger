d60166986ac16e5fcb619b1f5f37a2ffbfcb2cd5
masbagaspn <masbagaspn@gmail.com>
2023-05-14 13:53:09 +0700

    feat: loading modal component

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Avatar from "@/app/components/Avatar"
+import LoadingModal from "@/app/components/modals/LoadingModal"
 import { User } from "@prisma/client"
 import axios from "axios"
 import { useRouter } from "next/navigation"
@@ -27,13 +28,18 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
     }, [data, router])
 
   return (
-    <div
-        onClick={handleClick}
-        className="relative w-full flex items-center space-x-3 p-3 bg-white rounded-md transition cursor-pointer hover:bg-neutral-100"
-    >
-        <Avatar user={data} />
-        <p className="flex-1 focus:outline-none text-sm tracking-tight font-medium text-neutral-900 capitalize">{data.name}</p>
-    </div>
+    <>
+        {isLoading && (
+            <LoadingModal />
+        )}
+        <div
+            onClick={handleClick}
+            className="relative w-full flex items-center space-x-3 p-3 bg-white rounded-md transition cursor-pointer hover:bg-neutral-100"
+        >
+            <Avatar user={data} />
+            <p className="flex-1 focus:outline-none text-sm tracking-tight font-medium text-neutral-900 capitalize">{data.name}</p>
+        </div>
+    </>
   )
 }
 
